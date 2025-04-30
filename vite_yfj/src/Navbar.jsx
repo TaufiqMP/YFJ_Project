a4722dd2c166f0ed7770ccb91c7e7a5926937594
TaufiqMP <taufiqmaulanapr@gmail.com>
2025-04-30 22:50:43 +0700

    tailwind css

---
+++ vite_yfj/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Navbar() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-black text-white p-4">
+        <p>test</p>
+    </div>
+  )
+}
+
+export default Navbar
